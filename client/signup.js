79b13d127b5e7df1bd6d5faa7d00ea3eb15745ae
limabeans <toomanytypos@gmail.com>
2015-03-22 17:08:10 -0700

    trying to use autoform for signup.html

--- client/signup.js
+++ client/signup.js
@@ -8,45 +8,5 @@ Template.signup.helpers({
 if(Meteor.isClient) {
 
   Template.signup.events({
-    "input #nickname": function(event) {
-      console.log(event);
-    },
-    "change #box": function(event) {
-      var workshop = event.target.value;
-      var attending = event.target.checked;
-      Session.set(workshop, attending);
-    },
-    "click .btn": function(event) {
-      var nickname=document.getElementById('nickname').value;
-
-      var first_name=document.getElementById('first_name').value;
-      var last_name=document.getElementById('last_name').value;
-      var password=document.getElementById('password1').value;
-      var email=document.getElementById('email').value;
-      var phone_number=document.getElementById('phone_number').value;
-      var school=document.getElementById('school').value;
-
-      map = []
-      for (name in Session.keys) {
-        if(Session.keys[name] === "true") {
-          map.push({
-            name: name
-          });
-        }
-      }
-
-      Kids.insert({
-        nickname: nickname,
-        first_name: first_name,
-        last_name: last_name,
-        password: password,
-        email: email,
-        phone_number: phone_number,
-        school: school,
-        workshops: map
-      });
-
-      return false;
-    }
   });
 }
